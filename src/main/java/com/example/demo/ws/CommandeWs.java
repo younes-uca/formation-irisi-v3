7ac7d14284131635b9f812a796a752700ed5f27c
younes-uca <y.zouani@uca.ma>
2023-12-16 16:13:28 +0100

    init project

--- src/main/java/com/example/demo/ws/CommandeWs.java
+++ src/main/java/com/example/demo/ws/CommandeWs.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/command/")
+@RequestMapping("api/admin/commande/")
 public class CommandeWs {
     @Autowired
     private CommandeService commandeService;
---
+++ src/test/java/com/example/demo/service/CommandeTest.java
@@ -0,0 +1,49 @@
+package com.example.demo.service;
+
+
+import com.example.demo.bean.Commande;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class CommandeTest {
+    @Autowired
+    private MockMvc mockMvc; // to make requests to the ProductController
+    @Autowired
+    private ObjectMapper objectMapper; // convert the ProductRequest to String
+    @Autowired
+    private CommandeService service;
+    private String path= "http://localhost:8036/api/commande/";
+
+
+    @Test
+    void shouldCreateProduct() throws Exception {
+        Commande input = getInput();
+        String inputAsString = objectMapper.writeValueAsString(input);
+        mockMvc.perform(MockMvcRequestBuilders.post(path)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(inputAsString))
+                .andExpect(status().isCreated());
+        Assertions.assertEquals(2, service.findAll().size());
+    }
+
+    private Commande getInput() {
+        Commande input = new Commande();
+        input.setReference("c1");
+        input.setTotal(100);
+        input.setTotalPaye(10);
+        return input;
+
+    }
+}
